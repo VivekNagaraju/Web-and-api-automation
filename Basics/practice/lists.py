fa4003ff9d9c2e820ea12f868a32f694cd7687a3
admin <admin@DESKTOP-FDITP2P>
2023-06-13 08:58:17 +0530

    Committing the code of 13/6/23

---
+++ Basics/practice/lists.py
@@ -0,0 +1,15 @@
+a = [1, 2, 3, 4, 5]
+print(a)
+
+'''List is heterogenous'''
+b = [1, 2, 3.0, 4.556, "abc"]
+print(b)
+
+c=list(range(1, 6))
+print(c)
+print(type(c))
+
+print(b[-1])
+
+print(b[0:5:2])
+print(b[::-1])
