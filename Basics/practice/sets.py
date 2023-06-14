bf82f60d9303f856a4b84c7650db272052ce065d
admin <admin@DESKTOP-FDITP2P>
2023-06-14 09:02:27 +0530

    14-06-23

---
+++ Basics/practice/sets.py
@@ -0,0 +1,13 @@
+a={1, 2, 3, 5} #homogeneous
+print(type(a))
+
+b={1, 2.0, "abc"} #sets can be heterogeneous
+print(b)
+
+c=set(range(1, 6)) #built-in func
+print(c)
+
+e={1, 1, 3, 4, 5} # sets can't store duplicates
+print(e)
+
+
