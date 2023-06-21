7f73aa1332acc72e8adb903c3691dc0b2e78fae8
admin <admin@DESKTOP-FDITP2P>
2023-06-21 08:32:39 +0530

    21-06-2023

--- Basics/oops/intro.py
+++ Basics/oops/intro.py
@@ -16,14 +16,27 @@ class Sample:
 # print(dir(obj))
 
 class Student:
+    
+    school="abc" # class variables
     def __init__(self, name, roll_no):
-        self.name=name
+        self.name=name #instance/ object variables
         self.roll_no= roll_no
-        print(f"name of the student is {self.name} and roll no, is {self.roll_no}")
+        print(f"name of the student is {self.name} and roll no, is {self.roll_no} and school is {Student.school}")
         
     def display_details(self):
         print("This is display method")
         
+    def sum_of_subjects(self, kannada, english):
+        print(f"Total marks obtained by {self.name}",kannada+english) #method variables
         
-std=Student("vivek", 1)
+    
+        
+        
+std1=Student("vivek", 1)
+std1.name="Manju"
+std1.sum_of_subjects(100, 99)
+print(dir(std1))
+std2=Student("sheela", 2)
+print(std2.name)
+print(std1.name)
 # std.display_details()
