857b0d9f5adc40449e2e97d0c0da878ad9412632
admin <admin@DESKTOP-FDITP2P>
2023-06-27 08:28:36 +0530

    26-06-23, 27-06-23

---
+++ Basics/oops/practice_logging.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.debug("This is debug message")
+logging.info("This is info")
+logging.warning("This is warning")
+logging.error("This is error message")
+logging.fatal("This is fatal message")
