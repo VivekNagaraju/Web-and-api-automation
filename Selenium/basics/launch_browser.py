b78403f33d97a8fdb29a92b320493419dca70c3d
admin <admin@DESKTOP-FDITP2P>
2023-06-30 08:53:18 +0530

    30-06-23

--- Selenium/basics/launch_browser.py
+++ Selenium/basics/launch_browser.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+'''1. launching the chrome browser with detach option and maximising the window'''
+opts=webdriver.ChromeOptions()
+opts.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=opts)
+driver.maximize_window()
+# driver.implicitly_wait(10)
 
-driver = webdriver.Chrome()
+'''2. Navigating to target URL'''
+driver.get("https://testautomationpractice.blogspot.com/")
 
-driver.get("http://selenium.dev")
+'''3. Enter Name'''
+# driver.find_element(By.ID, "name").send_keys("Vivek") # not recommended
+name_txtbx=driver.find_element(By.XPATH, "//input")
+name_txtbx.send_keys("Vivek")
 
-driver.quit()
+'''4. Enter email'''
+email_txtbx=driver.find_element(By.XPATH, "//input[@id='email']")
+email_txtbx.send_keys("vivek@gmail.com")
