ce384164b37e442ebd7bb445b7c00fa1699c1c7c
admin <admin@DESKTOP-FDITP2P>
2023-07-11 08:36:32 +0530

    11-07-2023

--- Selenium/basics/launch_browser.py
+++ Selenium/basics/launch_browser.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 '''1. launching the chrome browser with detach option and maximising the window'''
 opts=webdriver.ChromeOptions()
